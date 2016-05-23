97cd0bae773e4542b73f7a062b874b23db788c53
robben1234 <c.gavrilenko.96@gmail.com>
2016-05-23 21:29:05 +0300

    toneMatrix

--- part1/src/com/shpp/havrylenko/cs/task6/tm/ToneMatrixLogic.java
+++ part1/src/com/shpp/havrylenko/cs/task6/tm/ToneMatrixLogic.java
@@ -13,7 +13,34 @@ public class ToneMatrixLogic {
     public static double[] matrixToMusic(boolean[][] toneMatrix, int column, double[][] samples) {
         double[] result = new double[ToneMatrixConstants.sampleSize()];
 
-		/* TODO: Fill this in! */
+        for(int i = 0; i < toneMatrix.length; i++) {
+            if(toneMatrix[i][column]) {
+                for(int j = 0; j < result.length; j++) {
+                    result[j] += samples[i][j];
+                }
+            }
+        }
+
+        double maxValue = 0.;
+        double minValue = 0.;
+        
+        for(double aResult : result) {
+            if(aResult > 1. || aResult < -1.) {
+                if(aResult > maxValue) maxValue = aResult;
+                else if(aResult < minValue) minValue = aResult;
+            }
+        }
+
+        double divider;
+        double approxMinMax = maxValue + minValue;
+
+        if(approxMinMax == 0) return result;
+        else if(approxMinMax > 0) divider = maxValue;
+        else divider = minValue;
+
+        for(int i = 0; i < result.length; i++) {
+            result[i] /= divider;
+        }
 
         return result;
     }
